3ba69af743c151a7d3b89314ded6b94f2603d1c8
L <465614842@qq.com>
2019-04-18 19:22:06 +0800

    增加自动生成C#和C++代码功能

---
+++ Example/Code/JsonReadObject.cs
@@ -0,0 +1,23 @@
+namespace JsonReadObject
+{
+public class 内门弟子
+{
+//NPC等级
+public uint Level;
+//NPC境界
+public string Jingjie;
+//血量
+public uint Blood;
+//攻击
+public uint Atk;
+//防御
+public uint Defense;
+//战斗力
+public uint FightingCap;
+//伤害加成
+public double ShanghaiJiacheng;
+//额外受伤
+public double ShoushangZengjia;
+}
+
+}
---
+++ Example/Code/JsonReadObject.h
@@ -0,0 +1,24 @@
+namespace JsonReadObject
+{
+public class 内门弟子
+{
+public:
+//NPC等级
+uint Level;
+//NPC境界
+string Jingjie;
+//血量
+uint Blood;
+//攻击
+uint Atk;
+//防御
+uint Defense;
+//战斗力
+uint FightingCap;
+//伤害加成
+double ShanghaiJiacheng;
+//额外受伤
+double ShoushangZengjia;
+}
+
+}
